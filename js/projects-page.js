b0efbec83177e019229adff17a6bc8e68073d08d
Ali Awais <raoawais.dev@gmail.com>
2026-07-18 12:56:53 +0500

    added temp console log to check searchbar functionality

--- js/projects-page.js
+++ js/projects-page.js
@@ -1,4 +1,17 @@
+const searchInput = document.getElementById("project-search");
 const projectsGrid = document.getElementById("projects-grid");
+
+if (searchInput) {
+
+    searchInput.addEventListener("input", function () {
+
+        console.log(this.value);
+
+    });
+
+}
+
+
 
 if (projectsGrid){
 
